6edb33b0a7d97a01e5a52c8ae44664b64bcb2f7f
LosmanDev <losman78@gmail.com>
2023-07-27 22:50:22 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <h1 className="crypto-title m-5 font-extrabold font-mono shadow-lg shadow-yellow-500/50">
+        The Crypto Suuq
+      </h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/CryptoApi.jsx
@@ -0,0 +1,9 @@
+// const key = 'f952657b-92a2-4750-80eb-89bf56ff9049'
+
+// import { useState, useEffect } from 'react';
+
+// const CryptoApi = () => {
+//     const [crypto, setCrypto] = useState();
+// }
+
+// export default CryptoApi;
