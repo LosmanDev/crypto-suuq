78a704dfe8a9ecfe65ad5a208f7f77265556f302
LosmanDev <losman78@gmail.com>
2023-08-08 16:35:26 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import "./App.css";
+import CryptoApp from "./CryptoApp";
 
 const App = () => {
-  return (
-    <div className="text-center">
-      <h1 className="crypto-title m-5 font-extrabold font-mono shadow-lg shadow-yellow-500/50">
-        The Crypto Suuq
-      </h1>
-    </div>
-  );
+  return <CryptoApp />;
 };
 
 export default App;
