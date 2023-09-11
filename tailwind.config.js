0421c4c5e16e408e667bca7256664b1bf7881531
LosmanDev <losman78@gmail.com>
2023-09-10 21:50:23 -0400

    fixed issue with styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
-/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: { keyframes: {
-      fadeIn: {
-        '0%': { opacity: 0 },
-        '100%': { opacity: 1 },
+    extend: {
+      width: {
+        30: "30px",
+        48: "48px",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        slideIn: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        fadeUp: {
+          "0%": { opacity: 0, transform: "translateY(20%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in-out",
+        slideIn: "slideIn .8s ease-in-out",
+        fadeUp: "fadeUp 1s ease-in-out",
       },
     },
-    animation: {
-      fadeIn: 'fadeIn 1s ease-in-out',
-    },},
   },
-  plugins: []
-}
+  // eslint-disable-next-line no-undef
+  // plugins: [require("daisyui")],
+};
